2b10834c2b1862d02af235854f6b9ca605865179
dolggul2 <dolggul2@gmail.com>
2025-02-07 16:47:54 +0900

    remove: Delete all FastAPI tutorial code.

--- main.py
+++ main.py
@@ -1,41 +1,16 @@
-from typing import Union
 import os
 
 from fastapi import FastAPI
 from fastapi import Request
-from pydantic import BaseModel
 from openai import OpenAI
 from dotenv import load_dotenv
 
+from prompt import PROMPT
 
 load_dotenv()
 app = FastAPI()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-class User(BaseModel):
-    name: str
-    email: str
-
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item, user: User):
-    return {"item_name": item.name, "item_price": item.price, "item_id": item_id, "user_name": user.name}
 
 @app.post("/slack/events")
 async def slack_events(request: Request):
