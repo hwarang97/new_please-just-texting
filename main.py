15493b9d3e2486a8a745b1143d1dfd98eecaa80f
dolggul2 <dolggul2@gmail.com>
2025-04-14 23:15:13 +0900

    feat: slack request_url verifying endpoint "slack/events"

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-from typing import Union
-
-from fastapi import FastAPI
-from fastapi import Request
-from pydantic import BaseModel
+from fastapi import FastAPI, Request
 
 app = FastAPI()
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-class User(BaseModel):
-    name: str
-    email: str
 
 
 @app.get("/")
@@ -21,26 +8,7 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item, user: User):
-    return {"item_name": item.name, "item_price": item.price, "item_id": item_id, "user_name": user.name}
-
 @app.post("/slack/events")
 async def slack_events(request: Request):
     data = await request.json()
-
-    if data.get("type") == "url_verification":
-        return {"challenge": data.get("challenge")}
-
-    if data.get("type") == "event_callback":
-        event = data.get("event")
-        print("message: ", event.get("text"))
-
-        return {"status": "ok"}
-
-    return {"status": "ignored"}
+    return {"challenge": data["challenge"]}
