f705f6593b3f03f050b9e2706d86831ba678edb7
dolggul2 <dolggul2@gmail.com>
2025-04-15 14:30:59 +0900

    feat: add event handler and ngrok to allow server to be run with python script.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import uvicorn
 from fastapi import FastAPI, Request
+from pyngrok import ngrok
 
 app = FastAPI()
 
@@ -11,4 +13,18 @@ async def read_root():
 @app.post("/slack/events")
 async def slack_events(request: Request):
     data = await request.json()
-    return {"challenge": data["challenge"]}
+    event_type = data.get("type")
+
+    if event_type == "event_callback":
+        print(data)
+        return {"status": "ok"}
+
+    if event_type == "url_verification":
+        return {"challenge": data["challenge"]}
+
+
+if __name__ == "__main__":
+    port = 8000
+    http_tunnel = ngrok.connect(addr=f"{port}", bind_tls=True)
+    print(f"ngrok url: {http_tunnel}")
+    uvicorn.run("main:app", port=port)
