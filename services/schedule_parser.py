a8a856e246c1865578d81e59adf9fe78b970a6a2
dolggul2 <dolggul2@gmail.com>
2025-04-16 16:14:48 +0900

    feat: pin model snapshots.
    - Set llm model version 2025-04-15.

--- services/schedule_parser.py
+++ services/schedule_parser.py
@@ -8,6 +8,8 @@ load_dotenv()
 API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=API_KEY)
 
-response = client.responses.create(model="gpt-4.1-nano", input="Hello, GPT!")
+# input에 들어가는 것을 jinja같은 템플렛을 이용해서 만들어보면 어떨까? 꼭 진자를 써야하나?
+
+response = client.responses.create(model="gpt-4.1-nano-2025-04-14", input="Hello, GPT!")
 
 print(response.output_text)
