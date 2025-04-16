edae120acd58ab9118db81e982c8700a0e17ee6e
dolggul2 <dolggul2@gmail.com>
2025-04-17 00:13:38 +0900

    feat: add slack message parsing function to event handler

--- services/schedule_parser.py
+++ services/schedule_parser.py
@@ -2,14 +2,23 @@ import os
 
 from dotenv import load_dotenv
 from openai import OpenAI
+from pathlib import Path
 
 load_dotenv()
 
 API_KEY = os.getenv("OPENAI_API_KEY")
+BASE_DIR = Path(__file__).resolve().parents[1]
+EXTRACT_PROMPT_PATH = BASE_DIR / "prompts" / "extract.xml"
+
 client = OpenAI(api_key=API_KEY)
 
-# input에 들어가는 것을 jinja같은 템플렛을 이용해서 만들어보면 어떨까? 꼭 진자를 써야하나?
+def parse_schedule_from_message(message: str):
+    with open(EXTRACT_PROMPT_PATH) as f:
+        prompt = f.read()
 
-response = client.responses.create(model="gpt-4.1-nano-2025-04-14", input="Hello, GPT!")
+    response = client.responses.create(
+        model="gpt-4.1-nano-2025-04-14",
+        instructions=prompt,
+        input=message)
 
-print(response.output_text)
+    return response.output_text
--- tasks/event_handler.py
+++ tasks/event_handler.py
@@ -1,3 +1,6 @@
+from services.schedule_parser import parse_schedule_from_message
+
 async def handle_event_creation(event_data: dict):
     message = event_data.get("event").get("text")
+    schedule_info = parse_schedule_from_message(message)
     print(message)
