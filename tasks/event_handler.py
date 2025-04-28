5e1d95a570ee2c78bb35041f9bfe9dffc7badd4d
dolggul2 <dolggul2@gmail.com>
2025-04-29 04:08:58 +0900

    feat: convert to parsed data to dict and add creating event code.
    - Add comments for logic flow.
    - Convert json string to dict.
    - Call create_event function to create event.
    - Add CRUD selection.

--- tasks/event_handler.py
+++ tasks/event_handler.py
@@ -1,6 +1,26 @@
+import json
+
+from services.create_event import create_event
 from services.schedule_parser import parse_schedule_from_message
 
-async def handle_event_creation(event_data: dict):
+
+async def handle_event(event_data: dict):
+    # Message parsing
     message = event_data.get("event").get("text")
-    schedule_info = parse_schedule_from_message(message)
-    print(schedule_info)
+    schedule_info = json.loads(parse_schedule_from_message(message))
+
+    # Event handle
+    # action = schedule_info.get("action", None)
+    action = "create"
+
+    # schedule_info 에서 CRUD 중 하나를 선택
+    if action == "create":
+        create_event(schedule_info)
+    elif action == "read":
+        pass
+    elif action == "update":
+        pass
+    elif action == "remove":
+        pass
+
+    # 사용자에게 작업이 완료되었음을 알림
