a15e5eca646e02a329dfb5037c4b836750860523
dolggul2 <dolggul2@gmail.com>
2025-05-16 04:04:38 +0900

    feat: send a message to channel.
    - Add send_message function to send message to slack channel.
    - call send_message function in read login.

--- tasks/event_handler.py
+++ tasks/event_handler.py
@@ -1,23 +1,27 @@
 import json
 
-from services.event_crud import create_event
+from services.event_crud import create_event, get_event
 from services.schedule_parser import parse_schedule_from_message
+import requests
 
+from services.slack_messanger import send_message
 
 async def handle_event(event_data: dict):
     # Message parsing
     message = event_data.get("event").get("text")
-    schedule_info = json.loads(parse_schedule_from_message(message))
+    # schedule_info = json.loads(parse_schedule_from_message(message))
+    schedule_info = {}
 
     # Event handle
     # action = schedule_info.get("action", None)
-    action = "create"
+    action = "read"
 
     # schedule_info 에서 CRUD 중 하나를 선택
     if action == "create":
         create_event(schedule_info)
     elif action == "read":
-        pass
+        event_list = get_event(schedule_info).get('items')
+        send_message(event_list)
     elif action == "update":
         pass
     elif action == "remove":
